c1f492d6e08fd1c725ed3531e1caf900e5ca1d92
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-05-14 17:13:22 -0430

    Change the directive controller

--- app/scripts/directives/todos.js
+++ app/scripts/directives/todos.js
@@ -1,11 +1,11 @@
 'use stict';
-
+var angular = require('angular');
 var app = angular.module('todoListApp');
 
 app.directive('todos', function() {
 	return {
 		templateUrl: 'templates/todo.html',
-		controller: 'mainCtrl',
-		replaced: true // Para reemplazar el tag 'todos' por el contenido de la plantilla.
+		controller: 'todoCtrl',
+		replaced: false // Para reemplazar el tag 'todos' por el contenido de la plantilla.
 	};
 });
