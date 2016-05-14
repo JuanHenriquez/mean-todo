036eb058fc648c4fa97f27ec2bd435c7e690a44e
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-05-14 17:10:25 -0430

    Change the patho to get data

--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -1,11 +1,11 @@
 'use strict';
-
+var angular = require('angular');
 var app = angular.module('todoListApp');
 
 app.service('dataService', function( $http ){
 
 	this.getTodos = function( callback ){
-		$http.get('mock/todos.json').then( callback );
+		$http.get('/api/todos').then( callback );
 	};
 
 	this.deleteTodo = function( todo ) {
