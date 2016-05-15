23ac5fd8e6f5b2ea23268d5c920ae7aa78a3eb2a
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-05-15 14:31:48 -0430

    Update the dataService Service to create and update todos task

--- public/scripts/todo.bundle.js
+++ public/scripts/todo.bundle.js
@@ -88,7 +88,7 @@ webpackJsonp([0],[
 	var angular = __webpack_require__(1);
 	var app = angular.module('todoListApp');
 
-	app.service('dataService', function( $http ){
+	app.service('dataService', function( $http, $q ){
 
 		this.getTodos = function( callback ){
 			$http.get('/api/todos').then( callback );
@@ -99,7 +99,40 @@ webpackJsonp([0],[
 		};
 
 		this.saveTodos = function( todos ) {
-			console.log(todos.length + ' todos have been saved.');
+	        // Array que contendra todos los request(solicitudes)
+	        // que se vayan hacer.
+			var queue = [];
+
+	        // Itero sobre cada todo.
+	        todos.forEach(function(todo) {
+	            // Inicializo la variable request.
+	            var request;
+
+	            // Si el todo en esta parte de la iteracion no tiene un id
+	            // quiere decir que no esta guardado en la base de datos.
+	            // Si no esta guardado, guardo una solicitud POST(la que lo guardara)
+	            //en la variable request.
+	            if(!todo._id) {
+	                // Guardo la solicitud. No la ejecuto todavia.
+	                request = $http.post('/api/todos', todo);
+
+	            // Si el todo si tiene una id, quiere decir que se actualiazara.
+	            } else {
+	                request = $http.put('/api/todos/' + todo._id, todo)
+	                .then(function(result) {
+	                    todo = result.data.todo;
+	                    return todo;
+	                });
+	            }
+	            // Guardo la solicitud en mi array de solicitudes para su
+	            // posterior ejecucion.
+	            queue.push(request);
+	        });
+
+	        $q.all(queue).then(function(results) {
+	            console.log("I saved " + todos.length + " todos.");
+	            console.log(queue);
+	        });
 		};
 
 	});
