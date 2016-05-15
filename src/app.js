577cb9335fc83b551dc876bd09e7d11121ab783e
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-05-15 14:29:46 -0430

    Install and use body-parser to parse the data inside the request body.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,18 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var router = require('./api');
 
 // Import mongodb DB.
 require('./database');
+require('./seed');
 
 // Coloca toda la carpeta public en la ruta principal,
 // por lo tanto se pueden acceder a las demas carpetas
 // automaticamente.
 app.use('/', express.static('public'));
+app.use(bodyParser.json());
 app.use('/api', router);
 
 
